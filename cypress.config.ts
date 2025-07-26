78dde2a42eb309ec5868c96276d200f0209738a4
Manojpanda3 <manojpandawork@gmail.com>
2025-07-26 11:57:47 +0530

    test(auth): Settingup cypress for E2E testing
    1. Installing cypress as dev dep
    2. Configuring cypress.
    
    SignedBy: github.com/ManojPanda3

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: process?.env?.BASE_URL ?? "http://localhost:3000",
+    setupNodeEvents(on, config) {
+    },
+  },
+});
